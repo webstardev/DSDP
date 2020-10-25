bb555cd24963d6c388e79f05ef444c41096a7b9b
whitebearcoder <denisvaran@outlook.com>
2020-10-25 10:53:02 -0700

    init commit

---
+++ pages/Articles.vue
@@ -0,0 +1,169 @@
+<template>
+  <div v-if="loaded">
+    <BannerTitle
+      title="Get your head out of the sand"
+      description="Stay informed and learn ways to protect your privacy"
+    />
+    <b-container>
+      <div class="small-container">
+        <b-card v-for="(post, index) in posts" :key="index">
+          <b-card-body>
+            <b-card-title>
+              <div class="caption mb-2">
+                {{ getPostDate(post.date) }}
+              </div>
+              <h2 class="text-center">
+                <NuxtLink :to="'/articles/' + post.slug">
+                  {{ post.title.rendered }}
+                </NuxtLink>
+              </h2>
+            </b-card-title>
+          </b-card-body>
+
+          <b-card-img
+            img-top
+            v-if="post._embedded && post._embedded['wp:featuredmedia']"
+            :src="post._embedded['wp:featuredmedia'][0].source_url"
+          />
+          <b-card-text
+            class="mt-4 f-Roboto"
+            v-html="post.excerpt ? post.excerpt.rendered : ''"
+          ></b-card-text>
+
+          <NuxtLink
+            :to="'/articles/' + post.slug"
+            class="card-link btn btn-outline f-Montserrat"
+          >
+            READ MORE
+            <b-icon-arrow-right></b-icon-arrow-right>
+          </NuxtLink>
+        </b-card>
+      </div>
+
+      <infinite-loading @infinite="infiniteHandler">
+        <div slot="no-more"></div>
+        <div slot="no-results">No articles found</div>
+      </infinite-loading>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import moment from "moment";
+
+export default {
+  head: {
+    title: "Deep State Defender Articles",
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: "Get your head out of the sand!"
+      }
+    ]
+  },
+  created() {
+    var self = this;
+    self.$nextTick(function() {
+      self.loaded = true;
+    });
+  },
+  layout: "pages",
+  data() {
+    // asyncData(context) {
+    return {
+      postsUrl: "https://wp.dsdefender.com/wp-json/wp/v2/posts",
+      queryOptions: {
+        per_page: 6,
+        page: 0,
+        _embed: true
+      },
+      // Returned Posts in an Array
+      posts: [],
+      loaded: false
+    };
+  },
+  methods: {
+    // Get Recent Posts From WordPress Site
+
+    infiniteHandler($state) {
+      this.queryOptions.page++;
+      this.getRecentMessages()
+        .then(response => {
+          this.posts = this.posts.concat(response.data);
+          if (response.data.length) {
+            $state.loaded();
+          } else {
+            $state.complete();
+          }
+        })
+        .catch(() => {
+          $state.complete();
+        });
+    },
+    getRecentMessages() {
+      return axios
+        .get(this.postsUrl, { params: this.queryOptions })
+        .then(response => {
+          return response;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+
+    getPostDate(date) {
+      return moment(date).format("lll");
+    }
+  },
+  mounted() {}
+};
+</script>
+
+<style lang="scss" scoped>
+.articles-container {
+  padding-top: 70px;
+  padding-bottom: 80px;
+  @media (max-width: 991px) {
+    padding-top: 30px;
+    padding-bottom: 40px;
+  }
+}
+.card {
+  background: transparent;
+  border: none;
+  .card-title {
+    color: #fff;
+    h2 {
+      font-size: 45px;
+    }
+    @media (max-width: 991px) {
+      h2 {
+        font-size: 24px;
+      }
+    }
+    a:hover {
+      color: #73fd05;
+    }
+    .caption {
+      color: #9ea1a6;
+      font-size: 14px;
+    }
+  }
+  .card-img-top {
+    width: 100%;
+    border-radius: 8px;
+    height: 289px;
+    object-fit: cover;
+  }
+  .card-text {
+    color: #9ea1a6;
+  }
+  .card-link {
+    .icon {
+      vertical-align: middle;
+    }
+  }
+}
+</style>
---
+++ pages/articles/_slug.vue
@@ -0,0 +1,245 @@
+<template>
+  <div>
+    <BannerTitle
+      title="Get your head out of the sand"
+      description="Stay informed and learn ways to protect your privacy"
+    />
+    <div class="articles-container small-container">
+      <b-card>
+        <b-card-body>
+          <b-card-img
+            img-top
+            v-if="post._embedded && post._embedded['wp:featuredmedia']"
+            :src="post._embedded['wp:featuredmedia'][0].source_url"
+          />
+
+          <b-card-title>
+            <div class="caption">
+              {{ getPostDate(post.date) }}
+            </div>
+            <h2>
+              {{ post.title ? post.title.rendered : "" }}
+            </h2>
+          </b-card-title>
+
+          <b-card-text
+            v-html="post.content ? post.content.rendered : ''"
+          ></b-card-text>
+
+        </b-card-body>
+      </b-card>
+    </div>
+
+    <div class="releated-post">
+      <div class="container">
+        <h2 class="section-title">More like this</h2>
+        <div class="row">
+          <div
+            class="col-sm-6 col-lg-3 mb-5 mb-lg-0"
+            v-for="(post, index) in releatedPosts"
+            :key="index"
+          >
+            <b-card>
+              <b-card-body>
+                <b-card-img
+                  img-top
+                  v-if="post._embedded && post._embedded['wp:featuredmedia']"
+                  :src="post._embedded['wp:featuredmedia'][0].source_url"
+                />
+                <b-card-title>
+                  <div class="caption">
+                    {{ getPostDate(post.date) }}
+                  </div>
+                  <h2 class="m-0">
+                    <NuxtLink :to="'/articles/' + post.slug">
+                      {{ post.title ? post.title.rendered : "" }}
+                    </NuxtLink>
+                  </h2>
+                </b-card-title>
+
+                <b-card-text
+                  v-html="post.excerpt ? post.excerpt.rendered : ''"
+                ></b-card-text>
+
+                <NuxtLink
+                  :to="'/articles/' + post.slug"
+                  class="card-link btn btn-outline f-Montserrat"
+                >
+                  READ MORE
+                  <b-icon-arrow-right></b-icon-arrow-right>
+                </NuxtLink>
+              </b-card-body>
+            </b-card>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import moment from "moment";
+
+export default {
+  layout: "pages",
+
+  data() {
+    return {
+      sharing: {},
+      post: {},
+      releatedPosts: []
+    };
+  },
+
+  head() {
+    return {
+      title: this.post.title ? this.post.title.rendered : "",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: this.post.excerpt ? this.post.excerpt.rendered : ""
+        }
+      ]
+    };
+  },
+
+  watch: {
+    "$route.params.slug": function() {
+      this.getRecentPost();
+    }
+  },
+
+  methods: {
+    // Get Recent Posts From WordPress Site
+    getRecentPost() {
+      const slug = this.$route.params.slug;
+      axios
+        .get(`https://wp.dsdefender.com/wp-json/wp/v2/posts?slug=${slug}`, {
+          params: {
+            per_page: 4,
+            page: 1,
+            context: "view",
+            _embed: true
+          }
+        })
+        .then(response => {
+          this.post = response.data[0] || {};
+
+          // console.log(this.post);
+
+          this.getReleatedPost();
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getReleatedPost() {
+      axios
+        .get(`https://wp.dsdefender.com/wp-json/wp/v2/posts`, {
+          params: {
+            per_page: 4,
+            page: 1,
+            _embed: true
+          }
+        })
+        .then(response => {
+          this.releatedPosts = response.data;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    getPostDate(date) {
+      return moment(date).format("lll");
+    }
+  },
+  mounted() {
+    this.getRecentPost();
+    this.getReleatedPost();
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.articles-container {
+  padding-top: 70px;
+  padding-bottom: 80px;
+}
+.card {
+  background: transparent;
+  border: none;
+  .card-title {
+    color: #fff;
+    h2 {
+      font-size: 45px;
+    }
+    a:hover {
+      color: #73fd05;
+    }
+    .caption {
+      color: #9ea1a6;
+      font-size: 14px;
+    }
+  }
+  .card-img-top {
+    width: 100%;
+    border-radius: 8px;
+    object-fit: cover;
+  }
+  .card-text {
+    color: #9ea1a6;
+  }
+  .card-link {
+    .icon {
+      vertical-align: middle;
+    }
+  }
+}
+.section-title {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  position: relative;
+  font-weight: 500;
+  font-size: 30px;
+  line-height: 48px;
+  text-transform: uppercase;
+  color: #9ea1a6;
+  margin-bottom: 70px;
+  @media (max-width: 991px) {
+    font-size: 24px;
+    margin-bottom: 30px;
+  }
+  &:before {
+    position: absolute;
+    top: 50%;
+    background: #9ea1a6;
+    left: -540px;
+    content: "";
+    width: 500px;
+    height: 1px;
+    display: table;
+  }
+}
+.releated-post {
+  background: #121314;
+  padding-top: 120px;
+  padding-bottom: 120px;
+  @media (max-width: 991px) {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+  .card-title {
+    h2 {
+      font-size: 24px;
+    }
+  }
+  .card-text {
+    font-size: 14px;
+  }
+  .card-img-top {
+    height: 100px;
+  }
+}
+</style>
